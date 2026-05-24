b0fbc744c82d0d2f60ff4f419fdb5262eef79118
Ramyasreeneralla12 <154748062+Ramyasreeneralla12@users.noreply.github.com>
2026-05-24 10:56:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,10 @@ import os
 
 class Config:
 
-    SECRET_KEY = "smart_secret_key"
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY",
+        "smart_secret_key"
+    )
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         "postgresql://taskdb_zd0i_user:6OsS9VMHOplTdLLlumyDtkPPtE0jOo7k@dpg-d8973uh9rddc73917gp0-a.oregon-postgres.render.com/taskdb_zd0i"
