841187020831a200d4eed76b6e882d13bfd53c56
Ramyasreeneralla12 <154748062+Ramyasreeneralla12@users.noreply.github.com>
2026-05-24 09:16:28 +0530

    Change socketio.run parameters for app

--- app.py
+++ app.py
@@ -44,7 +44,12 @@ app = create_app()
 if __name__ == "__main__":
 
     socketio.run(
-    app,
-    debug=True,
-    port=5001
-)
+
+        app,
+
+        host="0.0.0.0",
+
+        port=5000,
+
+        debug=True
+    )
